d4e16bf17e87370bc50b79b08ceeea5b4270c01a
Alex Baden <alex.baden@gmail.com>
2017-05-31 12:22:20 -0400

    Fix: Boss authentication fix for current neuroglancer implementation
    Converts fragment_id query argument to actual fragment.

--- client/ndviz/ui/default_viewer_setup.ts
+++ client/ndviz/ui/default_viewer_setup.ts
@@ -1,11 +1,13 @@
 import {makeDefaultKeyBindings} from 'neuroglancer/default_key_bindings';
 import {makeDefaultViewer} from 'ndviz/default_viewer';
 import {bindDefaultCopyHandler, bindDefaultPasteHandler} from 'neuroglancer/ui/default_clipboard_handling';
-import {UrlHashBinding} from 'neuroglancer/ui/url_hash_binding';
+import {UrlHashBinding, parseHashFromQueryString} from 'neuroglancer/ui/url_hash_binding';
 
 export function setupDefaultViewer() {
     let viewer = (<any>window)['viewer'] = makeDefaultViewer(); 
     makeDefaultKeyBindings(viewer.keyMap);
+
+    parseHashFromQueryString();
 
     const hashBinding = new UrlHashBinding(viewer.state);
     hashBinding.updateFromUrlHash();
